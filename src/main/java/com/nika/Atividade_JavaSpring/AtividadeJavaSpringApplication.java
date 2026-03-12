3d46ec12b8079a10e1834a8afbedc2471ba26842
Nicolas Kauan <166877999+NicolasKauan@users.noreply.github.com>
2026-03-12 19:27:27 -0300

    first commit

---
+++ src/main/java/com/nika/Atividade_JavaSpring/AtividadeJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.nika.Atividade_JavaSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nika/Atividade_JavaSpring/AtividadeJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nika.Atividade_JavaSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
